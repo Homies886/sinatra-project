46748e0de5304527cc274c7c58470b2230acf921
Nicholas Rodriguez <niko876@hotmail.com>
2019-07-19 16:49:16 +0000

    more minor edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/users/:id' do
-    @user = User.find_by_slug(params[:slug])
+    @user = User.find_by_id(params[:id])
     erb :'users/show'
   end
 
